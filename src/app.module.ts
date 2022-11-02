86d1bcb79290a1b7b7d40f7a97320b102548080d
Silas Tavares <silas.tavares07@gmail.com>
2022-11-02 20:00:05 -0300

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './postagem/entities/postagem.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '81485931',
+      database: 'db_blogpessoal',
+      entities: [Postagem],
+      synchronize: true
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
